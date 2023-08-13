61b1968295b1d75fc81be51ad79defe0f706773f
Niranjan <nrjsingh5123@gmail.com>
2023-08-13 14:22:32 +0530

    commited

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mobile_nav.addEventListener("click" , () =>{
 
 const p_btns = document.querySelector(".p-btns");
 const p_btn = document.querySelectorAll(".p-btn");
-const p_img_elem = document.querySelectorAll("img-overlay");
+const p_img_elem = document.querySelectorAll(".img-overlay");
 
 p_btns.addEventListener("click",(e) =>{
     const p_btn_clicked = e.target;
